2564420009bf042032312fd839dbf9ecfeb80f3f
jamiamikko <jamiamikko@gmail.com>
2020-04-01 20:22:25 +0300

    Check if books exist

--- src/components/BookList/BookList.vue
+++ src/components/BookList/BookList.vue
@@ -1,11 +1,20 @@
 <template>
   <section class="book-list">
     <div class="book-list__wrapper">
-      <Book
-        v-for="book in books"
-        v-bind:key="book.cover_i"
-        v-bind:title="book.title_suggest"
-      />
+      <div v-if="books.length">
+        <Book
+          v-for="book in books"
+          v-bind:key="`${book.author_key[0]}-${book.key}`"
+          v-bind:title="book.title_suggest"
+        />
+      </div>
+      <div class="book-list__introduction" v-else>
+        <h2>Hi there!</h2>
+        <p>Welcome to Book Browser!</p>
+        <p>
+          Start searching for interesting books by typing in the search bar.
+        </p>
+      </div>
     </div>
   </section>
 </template>
@@ -24,14 +33,20 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+@import '../../assets/scss/_variables.scss';
+
 .book-list {
-  border-bottom: 1px solid black;
   padding: 2rem 1rem;
+  border-bottom: 2px solid $gray-3;
 
   &__wrapper {
     width: 80%;
     margin: 0 auto;
     max-width: 75rem;
+  }
+
+  &__introduction {
+    text-align: center;
   }
 }
 </style>
--- src/components/SearchBar/SearchBar.vue
+++ src/components/SearchBar/SearchBar.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-  name: "SearchBar",
+  name: 'SearchBar',
   props: {}
 };
 </script>

70af45eaf5a7d3c1a7a5d5faf25725e4bf0be814
jamiamikko <jamiamikko@gmail.com>
2020-04-05 18:40:34 +0300

    First test

---
+++ tests/unit/app.spec.js
@@ -0,0 +1,27 @@
+import { shallowMount } from '@vue/test-utils';
+import App from '../../src/App.vue';
+import TheHeader from '../../src/components/TheHeader/TheHeader';
+import BookList from '../../src/components/BookList/BookList';
+import TheFooter from '../../src/components/TheFooter/TheFooter';
+
+let wrapper;
+
+describe('App.vue', () => {
+  beforeEach(() => {
+    wrapper = shallowMount(App);
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  it('has child components', () => {
+    expect(wrapper.find(TheHeader)).toBeTruthy();
+    expect(wrapper.find(BookList)).toBeTruthy();
+    expect(wrapper.find(TheFooter)).toBeTruthy();
+  });
+});
